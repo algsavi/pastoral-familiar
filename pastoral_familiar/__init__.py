f26ab13aa2d95cd973104c2e5398c8b97d12aa59
André Savi <asavi@its.jnj.com>
2016-01-25 17:15:58 -0200

    criacao de rota padrao

--- pastoral_familiar/__init__.py
+++ pastoral_familiar/__init__.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from flask import Flask, current_app, render_template
+from flask import Flask, current_app, render_template, redirect, url_for
 from .nucleo.views import nucleo
 from .agente.views import agente
 from extensions import db, lm, Principal, identity_loaded, Identity, UserNeed, RoleNeed
@@ -14,6 +14,10 @@ app.register_blueprint(nucleo, url_prefix='/nucleo')
 db.init_app(app)
 lm.init_app(app)
 principals = Principal(app)
+
+@app.route('/')
+def app_redirect():
+    return redirect(url_for('agente.index'))
 
 @app.errorhandler(403)
 def page_not_found(e):
