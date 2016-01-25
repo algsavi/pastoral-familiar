f3ab4552814850207c2c17a8eedacf516e407627
André Savi <asavi@its.jnj.com>
2016-01-25 16:02:40 -0200

    Alterar proxy

--- run.py
+++ run.py
@@ -2,6 +2,9 @@
 # -*- coding: utf-8 -*-
 
 from pastoral_familiar import app
+from werkzeug.contrib.fixers import ProxyFix
+
+app.wsgi_app = ProxyFix(app.wsgi_app)
 
 if __name__ == "__main__":
     app.run(debug=True)
