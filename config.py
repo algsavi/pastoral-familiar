b4e7097ccb73c3d1377bdda855dde8f2e5705b20
André Savi <asavi@its.jnj.com>
2016-02-12 09:21:24 -0200

    Alteração no template de emails security

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
+DEBUG = False
 CSRF_ENABLED = True
 SECRET_KEY = 'XIkJIUYjJLKWQq0921'
 
@@ -17,17 +18,20 @@ SECURITY_CHANGE_URL = '/trocar_senha'
 SECURITY_RESET_URL = '/reset_senha'
 SECURITY_LOGIN_URL = '/login'
 SECURITY_EMAIL_SUBJECT_PASSWORD_RESET = 'Instruções para definição de nova senha'
+SECURITY_EMAIL_SUBJECT_PASSWORD_NOTICE = 'Sua senha foi alterada'
 SECURITY_POST_LOGIN_VIEW = 'agente'
 SECURITY_PASSWORD_HASH = 'pbkdf2_sha512'
 SECURITY_PASSWORD_SALT = '6e95b1ed-a8c3-4da0-8bac-6fcb11c39ab4'
-SECURITY_MSG_INVALID_PASSWORD = (u'Senha inválida', 'error')
-SECURITY_MSG_USER_DOES_NOT_EXIST = (u'Usuário inválido', 'error')
-SECURITY_MSG_PASSWORD_NOT_PROVIDED = (u"Senha não fornecida", "error")
-SECURITY_MSG_EMAIL_NOT_PROVIDED = (u"E-mail não fornecido", "error")
-SECURITY_MSG_RETYPE_PASSWORD_MISMATCH = (u"As senhas não conferem", "error")
-SECURITY_MSG_PASSWORD_IS_THE_SAME = (u"A nova senha precisa ser diferente da senha anterior", "error")
-SECURITY_MSG_LOGIN = (u"Por favor, faça o login para acessar a página", "error")
-SECURITY_MSG_INVALID_RESET_PASSWORD_TOKEN = (u"Token inválido", "error")
+SECURITY_MSG_INVALID_PASSWORD = (u'Senha inválida', 'danger')
+SECURITY_MSG_USER_DOES_NOT_EXIST = (u'Usuário inválido', 'danger')
+SECURITY_MSG_PASSWORD_NOT_PROVIDED = (u"Senha não fornecida", "danger")
+SECURITY_MSG_EMAIL_NOT_PROVIDED = (u"E-mail não fornecido", "danger")
+SECURITY_MSG_RETYPE_PASSWORD_MISMATCH = (u"As senhas não conferem", "danger")
+SECURITY_MSG_PASSWORD_IS_THE_SAME = (u"A nova senha precisa ser diferente da senha anterior", "danger")
+SECURITY_MSG_LOGIN = (u"Por favor, faça o login para acessar a página", "danger")
+SECURITY_MSG_INVALID_RESET_PASSWORD_TOKEN = (u"Token inválido", "danger")
+SECURITY_MSG_PASSWORD_RESET = (u'Sua senha foi redefinida com sucesso.', 'success')
+SECURITY_MSG_PASSWORD_RESET_REQUEST = (u'As instruções para alterar sua senha foram enviadas para o seu e-mail', 'success')
 
 MAIL_SERVER = '50.56.21.178'
 MAIL_PORT = 465
